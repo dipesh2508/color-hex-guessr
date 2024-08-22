b490b2f6eed628553cb4f4d93a26fa34f633d85b
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-08-22 23:37:57 +0530

    feat:added user input color mapping and checking logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,84 +1,113 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { getRandomHexColor } from "@/lib/helpers";
-import * as z from 'zod';
-import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 const hexCodeSchema = z.object({
-  hexCode: z
-    .string()
-    .regex(/^[0-9a-fA-F]{6}$/, 'Invalid hex code.')
+  hexCode: z.string().regex(/^[0-9a-fA-F]{6}$/, "Invalid hex code."),
 });
 
 type FormData = z.infer<typeof hexCodeSchema>;
 
+interface ColorArray {
+  singleHex: string;
+  correct: boolean;
+}
+
 export default function Home() {
-  const [color, setColor] = useState<string>('#FFFFFF');
+  const [color, setColor] = useState<string>("#FFFFFF");
+  const [userColor, setUserColor] = useState<string>("");
+  const [colorArray, setColorArray] = useState<ColorArray[][]>();
 
   const form = useForm<FormData>({
     resolver: zodResolver(hexCodeSchema),
     defaultValues: {
-      hexCode: ''
-    }
+      hexCode: "",
+    },
   });
+
+  const { reset } = form;
 
   useEffect(() => {
     setColor(getRandomHexColor());
   }, []);
 
-  function handleClick(){
-    setColor(getRandomHexColor());
-  }
-
-  function checkHex(data:FormData){
+  function checkHex(data: FormData):ColorArray[] {
     const userHex = data.hexCode.toLowerCase();
-    const results = color.split('').map((char, index) => {
-      const userChar = userHex[index];
-      if (userChar === char) {
-        return { status: 'correct', char: userChar };
-      } else if (userChar < char) {
-        return { status: 'less', char: userChar };
-      } else {
-        return { status: 'more', char: userChar };
-      }
-    });
+    const colorHex = color.toLowerCase().slice(1);
+    const colorArray:ColorArray[] = [];
+    for (let i = 0; i < 6; i++) {
+      const singleHex = userHex.slice(i, i + 1);
+      const correct = singleHex === colorHex.slice(i, i + 1);
+      colorArray.push({ singleHex, correct });
+    }
+    return colorArray;
   }
 
   const onSubmit = (data: FormData) => {
-    checkHex(data);
-  }
+    const results = checkHex(data);
+    // add the array to the map state
+    setColorArray((prev) => {
+      if (!prev) {
+        return [results];
+      }
+      return [...prev, results];
+    });
+
+    console.log(colorArray);
+    setUserColor("#" + data.hexCode);
+    reset();
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={handleClick}>Click</Button>
-      <div className="size-64 flex items-center justify-center" style={{ backgroundColor: color }}>
+      <div className="flex flex-row gap-8">
+        <div
+          className="size-64 flex items-center justify-center border"
+          style={{ backgroundColor: color }}
+        ></div>
+        {/* user color box */}
+        <div
+          className="size-64 flex items-center justify-center border"
+          style={{ backgroundColor: userColor }}
+        ></div>
       </div>
       <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-row space-x-4 items-end">
-        <FormField
-          control={form.control}
-          name="hexCode"
-          render={({ field }) => (
-            <FormItem className="flex flex-col space-x-2 items-end">
-              <div className="flex space-x-2 items-end">
-
-              <FormLabel className="text-3xl">#</FormLabel>
-              <FormControl>
-                <Input placeholder="enter you guess" {...field} />
-              </FormControl>
-              </div>
-              <FormMessage/>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-row space-x-4 items-end"
+        >
+          <FormField
+            control={form.control}
+            name="hexCode"
+            render={({ field }) => (
+              <FormItem className="flex flex-col space-x-2 items-end">
+                <div className="flex space-x-2 items-end">
+                  <FormLabel className="text-3xl">#</FormLabel>
+                  <FormControl>
+                    <Input placeholder="enter you guess" {...field} />
+                  </FormControl>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </main>
   );
 }
